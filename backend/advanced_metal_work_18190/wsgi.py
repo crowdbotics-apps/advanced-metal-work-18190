f46cd961c43908d1fc225dbb0020ce204a93790c
Crowdbotics <team@crowdbotics.com>
2020-06-18 13:52:32 +0000

    Initial commit.

---
+++ backend/advanced_metal_work_18190/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for advanced_metal_work_18190 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_metal_work_18190.settings')
+
+application = get_wsgi_application()
